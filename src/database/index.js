72305554207cf18b156f6bac2421668dd82bb633
Marcílio Feitoza <43119325+marciliofeitoza@users.noreply.github.com>
2021-03-07 12:01:35 -0300

    Delete src/database directory

--- src/database/index.js
+++
@@ -1,8 +0,0 @@
-// const mongoose = require('mongoose')
-
-// mongoose.connect('mongodb://localhost:27017/electronics-store', {useNewUrlParser: true, useUnifiedTopology: true})
-// mongoose.Promise = global.Promise
-
-// // mongoose.connect('mongodb+srv://feitoza:f24software@cluster0.rudqr.mongodb.net/teste?retryWrites=true&w=majority', { useNewUrlParser: true })
-
-// module.exports = mongoose
